d3059b72d0ebcea8ab958dc6917e0a8b349bcce1
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-05-29 11:02:10 -0700

    .

---
+++ JavaScript/Asterisks.js
@@ -0,0 +1,13 @@
+let star = "*";
+
+for(let count = 1; count < 8; count++){
+	
+	let shape = " ";
+	for(let counter = 0; counter < count; counter++){
+	shape = shape + star;
+
+}
+
+	console.log(shape)
+
+};
---
+++ JavaScript/FizzBuzz.js
@@ -0,0 +1,22 @@
+let number = 0;
+
+for(let count = 1; count < 100; count++){
+	if (number % 3 == 0){
+	console.log("Fizz");
+
+}
+
+	if (number % 5 == 0){
+	console.log("Buzz");
+
+}
+
+	if(number % 3 == 0 && number % 5 == 0){
+	console.log("FizzBuzz");
+
+
+}
+
+
+
+}
