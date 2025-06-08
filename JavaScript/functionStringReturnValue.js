78c5b67a6f3ac2b0de745b6c7019ecdfc128a50d
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-06-08 01:12:57 -0700

    .

---
+++ JavaScript/functionStringReturnValue.js
@@ -0,0 +1,12 @@
+function functionString() {
+	let string = 'JavaScript is object oriented language';
+
+	let value = string.charAt(0);
+	let value1 = string.charAt(4);
+
+	console.log(value);
+	console.log(value1);
+
+}
+
+functionString()
