8dc0661d6df308c831e27f50e3968b8e286adf1e
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-07-24 02:30:35 +0100

    Add files via upload

--- JavaScript/functionForTest.js
+++ JavaScript/functionForTest.js
@@ -1,16 +1,38 @@
 
-function getEvenNumbers(array){
+// function getEvenNumbers(array){
+//     let newArray = [];
+//     for(let count= 0;count < array.length;count++){
+//             if(array[count] % 2 === 0){
+//                 newArray.push(array[count]);
+//             }
+//         }
+//         return newArray;
+//
+//
+// }
 
-    for(let count= 0;count<array.length;count++){
-        for (let counter = 0; counter < array.length; counter++){
-            if(array[count] % 2 == 0){
-                return array[count];
-            }
-        }
-        //return array[count];
-    }
 
+function  getEvenNumbers(array) {
+    return array.filter((num) => (num % 2 === 0));
 }
+const createCounter = function (params) {
+    let count = 0;
+    return {
+        getCount:()=>{
+            return count;
+        },
+        increment:()=>{
+            count += 1;
+        },
+        decrement:()=>{
+            count -= 1;
+        },
+        reset:()=>{
+            count = 0;
+        }
+    }
+}
+module.exports = {getEvenNumbers, createCounter}
 
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-console.log(getEvenNumbers(numbers));
+//let numbers = [1,2,3,4,5,6,7,8,9,10];
+//console.log(getEvenNumbers(numbers));
--- JavaScript/index.test.js
+++ JavaScript/index.test.js
@@ -1,9 +1,37 @@
-const getEvenNumbers = require("./functionForTest")
+const {getEvenNumbers, createCounter} = require("./functionForTest")
 
 
 test("that the function returns even numbers", ()=>{
     const arrayOfNumbers = [1,2,3,4,5,6,7,8,9,10]; // Arrange
     const result = getEvenNumbers(arrayOfNumbers); // Act
     expect(result).toEqual([2,4,6,8,10]);
-
+})
+describe("createCounter", () =>{
+    test("that the initial count is 0", ()=>{
+        const counter =  createCounter();
+        expect(counter.getCount()).toBe(0);
+    })
+    test("that the count increases by 1", ()=>{
+        const counter =  createCounter();
+        counter.increment()
+        expect(counter.getCount()).toBe(1);
+    })
+    test("that the count increases by 2", ()=>{
+        const counter =  createCounter();
+        counter.increment()
+        counter.increment()
+        expect(counter.getCount()).toBe(2);
+    })
+    test("that the count decreases by 2", ()=>{
+        const counter =  createCounter();
+        counter.increment()
+        counter.decrement()
+        expect(counter.getCount()).toBe(0);
+    })
+    test("that the count reset to 0 ", ()=>{
+        const counter =  createCounter();
+        counter.increment()
+        counter.reset()
+        expect(counter.getCount()).toBe(0);
+    })
 })
